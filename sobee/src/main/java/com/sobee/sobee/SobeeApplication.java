82bbbdaccff6a545b0d93faa3953e00fe2aff02a
thdudgus <gusthdud527@cau.ac.kr>
2026-05-19 12:02:20 +0900

    feat: 백엔드 공통 구조 추가

---
+++ sobee/src/main/java/com/sobee/sobee/SobeeApplication.java
@@ -0,0 +1,13 @@
+package com.sobee.sobee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SobeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SobeeApplication.class, args);
+	}
+
+}
---
+++ sobee/src/test/java/com/sobee/sobee/SobeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sobee.sobee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SobeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
